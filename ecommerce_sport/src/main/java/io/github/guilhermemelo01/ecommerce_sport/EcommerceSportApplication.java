3922fb54dc785abf1874c881471e2cf8529b5401
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2022-10-27 11:38:03 -0300

    projeto base e conexao mysql

---
+++ ecommerce_sport/src/main/java/io/github/guilhermemelo01/ecommerce_sport/EcommerceSportApplication.java
@@ -0,0 +1,13 @@
+package io.github.guilhermemelo01.ecommerce_sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSportApplication.class, args);
+	}
+
+}
---
+++ ecommerce_sport/src/test/java/io/github/guilhermemelo01/ecommerce_sport/EcommerceSportApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermemelo01.ecommerce_sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
